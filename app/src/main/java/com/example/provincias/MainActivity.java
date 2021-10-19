b648783a251efeed59d2b452fd4da50141fa32b3
dpriegoo <77279312+dpriegoo@users.noreply.github.com>
2021-10-19 14:48:10 +0200

    Clase

---
+++ app/src/main/java/com/example/provincias/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.provincias;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Spinner lstProvincias = findViewById(R.id.spinner);
+        ImageView sevilla = findViewById(R.id.sevillaView);
+    }
+
+    private void provincias (Spinner lstProvincias, ImageView sevilla){
+
+        lstProvincias.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+}
